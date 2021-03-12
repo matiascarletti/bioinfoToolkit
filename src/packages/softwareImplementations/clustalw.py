02c2fa03f1fb18c61f73111401c9f580ca0bf854
Matias Carletti <matias.carletti@gmail.com>
2021-03-12 00:58:27 -0300

    clustalw implentation added

---
+++ src/packages/softwareImplementations/clustalw.py
@@ -0,0 +1,22 @@
+import subprocess
+import os
+
+def clustalw_getMultipleSequenceAlignementFrom(mfasta,
+                                                outFolder,
+                                                outMsaName,
+                                                outFormat):
+    """
+    Purpose: Command line for run clustalw
+
+    Parameters:
+    -INFILE=archivo_de_entrada (este no puede tener espacios) 
+    -OUTFILE=archivo_de_salida 
+    -OUTPUT=fasta (-OUTPUT te da la opcion de elegir el formato de salida. Puede ser fasta, phylip, etc)
+    """
+    subprocess.call(["clustalw",
+                    "-INFILE=", mfasta,
+                    "-OUTFILE=", "%s/%s" % (outFolder, outMsaName),
+                    "-OUTPUT=", outFormat
+                    ], shell=False)
+    return
+    
